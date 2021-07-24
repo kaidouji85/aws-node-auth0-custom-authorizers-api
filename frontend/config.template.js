2d06409e1f0674fdeb8d37b62129eb06cbe831aa
竹内佑介 <kaidouji85@gmail.com>
2021-07-24 12:16:34 +0900

    フロント側の設定を別ファイルに切り出した

---
+++ frontend/config.template.js
@@ -0,0 +1,10 @@
+// auth0 クライアントID
+const AUTH0_CLIENT_ID = '';
+// auth0 ドメイン
+const AUTH0_DOMAIN = '';
+// auth0 コールバックURL
+const AUTH0_CALLBACK_URL = '';
+// aws api gateway パブリックAPIのエンドポイント
+const PUBLIC_ENDPOINT = '';
+// aws api gateway プライベートAPIのエンドポイント
+const PRIVATE_ENDPOINT = '';
